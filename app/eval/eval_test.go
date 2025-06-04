b54619f625e78249d4fe06dc96e66e80e5912010
Udeshya Dhungana <udeshya@Udeshyas-MacBook-Air-2.local>
2025-06-04 22:19:50 +0545

    fix: rename len->lambai_muji

--- app/eval/eval_test.go
+++ app/eval/eval_test.go
@@ -329,12 +329,12 @@ func TestBuiltinFunctions(t *testing.T) {
 		input    string
 		expected interface{}
 	}{
-		{`len("")`, 0},
-		{`len("four")`, 4},
-		{`len("hello world")`, 11},
-		{`len(1)`, "argument to `len` not supported, got INTEGER"},
-		{`len("one", "two")`, "wrong number of arguments. got=2, want=1"},
-		{`len([1,2,4])`, 3},
+		{`lambai_muji("")`, 0},
+		{`lambai_muji("four")`, 4},
+		{`lambai_muji("hello world")`, 11},
+		{`lambai_muji(1)`, "argument to `len` not supported, got INTEGER"},
+		{`lambai_muji("one", "two")`, "wrong number of arguments. got=2, want=1"},
+		{`lambai_muji([1,2,4])`, 3},
 	}
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
